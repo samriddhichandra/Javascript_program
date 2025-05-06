17af84215c3bab5397a32dc12e5b31da41353b0b
samriddhichandra <samriddhic62@gmail.com>
2025-05-06 19:05:03 +0530

    changes done
    changes done

---
+++ datatype.js
@@ -0,0 +1,4 @@
+console.log("DATA TYPE:");
+var myName = "Samriddhi Chandra"; // String
+var result = typeof myName; // String
+console.log("Data type of myName is:", result); // String
--- next.js
+++ next.js
@@ -1,1 +1,56 @@
-console.log("Samriddhi");
+// 01-variable-assignment.js
+
+// Logging personal info on console using var
+var city = "Pune";
+var bestFriend = "Samriddhi";
+var country = "India";
+var school = "Carmel School";
+var college = "Dr D.Y. Patil College of Engineering, Pune";
+
+console.log("======Asignment 1=======");
+console.log("City:", city);
+console.log("Best Friend:", bestFriend);
+console.log("Country:", country);
+console.log("School:", school);
+console.log("College:", college);
+
+
+
+
+
+//agnt 2
+// 1. Declare a variable with name → myFullName
+var myFullName;
+console.log("======Asignment 2=======");
+// Log the variable before initialization
+console.log("Before initialization:", myFullName);
+
+// Initialize with first and last name only
+myFullName = "Samriddhi ";
+console.log("After initialization:", myFullName);
+
+// Update with full name: first, middle, last
+myFullName = "Samriddhi Chandra";
+console.log("After update:", myFullName);
+
+// 2. Declare and initialize a variable with name → myLove
+var myLove = "I love only JS";
+console.log("myLove:", myLove);
+
+// 3. Declare and initialize variables for form controls on same line
+var firstName = "Samriddhi", 
+    lastName = "Chandra", 
+    age = 20, 
+    address = "Pune, Maharashtra", 
+    pinCode = "123456", 
+    gender = "Female", 
+    isMarried = false;
+
+// Log each variable
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("Pin Code:", pinCode);
+console.log("Gender:", gender);
+console.log("Is Married:", isMarried);
