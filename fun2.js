88e6fec63f4623a0ccde357f8580ed4dff29063a
samriddhichandra <samriddhic62@gmail.com>
2025-05-08 12:38:44 +0530

    created fun2

---
+++ fun2.js
@@ -0,0 +1,10 @@
+function bankDetails(bankName,accountNum,location,pinCode){
+    console.log(`Bank Name: ${bankName}, Account Number: ${accountNum}, Location: ${location}, PIN Code: ${pinCode}`);
+}
+
+
+bankDetails("CITI Bank", 234783839 , "Pune", 411057);
+bankDetails("HDFC Bank", 234783839 , "Mumbai", 411057);
+bankDetails("ICICI Bank", 234783839 , "Delhi", 411057);
+bankDetails("SBI Bank", 234783839 , "Banglore", 411057);
+bankDetails("Axis Bank", 234783839 , "Chennai", 411057);
