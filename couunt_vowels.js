16d386d3a43872742624086bb622fea2b3fd7ecd
samriddhichandra <samriddhic62@gmail.com>
2025-06-04 14:35:06 +0530

    new

---
+++ couunt_vowels.js
@@ -0,0 +1,40 @@
+//Goal: Count the vowels (both lowercase and uppercase): A, E, I, O, U, a, e, i, o, u
+let str ="I am a good IT developer";
+function countVowels(str){
+    let count = 0;
+    let vowels = "aeiouAEIOU";
+    for (let i = 0; i< str.length; i++){
+     if (vowels.includes(str[i])){
+         count++;
+       }
+    }
+    console.log("Number of vowels in the string: " + count);
+}
+countVowels(str);
+
+//2. Write a function to get the sum of cubes from 1 to 5
+ function sumOfCubes(n) {
+    let sum = 0;
+    for (let i =0; i <= n; i++) {
+        sum += Math.pow(i, 3);
+    }
+    console.log("Sum of cubes from 1 to " + n + " is: " + sum);
+}
+sumOfCubes(5);
+
+//Write a function oddPositionedChars
+function oddPositionedChars(input)
+{
+    console.log("Characters at odd positions in the string:");
+    console.log("Odd positioned characters (ignoring spaces):");
+    for(let i = 0; i< input.length; i++) {
+        if (i % 2 !== 0 && input[i] !== ' ') {
+            console.log(input[i]);
+        }
+    }
+}
+let string1 = "Hard work always pays back";
+let string2 = "Soon I will be UI IT Champ";
+
+oddPositionedChars(string1);
+oddPositionedChars(string2);
