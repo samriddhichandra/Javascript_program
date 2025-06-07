75a00a081957e7895d155c7a36a5998c741d4b7e
samriddhichandra <samriddhic62@gmail.com>
2025-06-07 12:44:24 +0530

    new

---
+++ Date.js
@@ -0,0 +1,16 @@
+let myDate = new Date();
+console.log(myDate.toString());
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr[1]);
+
+// object literals 
+const person = {
+  name: "John",
+  age: 30,
+  "occupation": "Engineer",
+  city: "New York"
+};
+console.log(person["name"]);
+console.log(person.occupation);
+ 
---
+++ obj.js
@@ -0,0 +1,7 @@
+//const tinderUser = new Object();
+ const tinderUser = {}
+ tinderUser.id = "123abc"
+ tinderUser.name = "Sammy"
+ tinderUser.isLoggedIn =false
+ console.log(tinderUser);
+  
